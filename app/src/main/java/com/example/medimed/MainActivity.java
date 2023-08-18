40f84d7a2f34771be21ae20739a5f9aa7ab51664
mrigankahazarika <hmrigank@gmail.com>
2023-08-18 23:50:48 +0530

    mk

---
+++ app/src/main/java/com/example/medimed/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.medimed;
+import android.app.ProgressDialog;
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.EditText;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading Data...");
+        progressDialog.setCancelable(false);
+        WebView web_view = findViewById(R.id.webview);
+        web_view.requestFocus();
+        web_view.getSettings().setLightTouchEnabled(true);
+        web_view.getSettings().setJavaScriptEnabled(true);
+        web_view.getSettings().setGeolocationEnabled(true);
+        web_view.loadUrl("https://webinfoghy.co.in/medimed/public/");
+        web_view.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                if (progress < 100) {
+                    progressDialog.show();
+                }
+                if (progress == 100) {
+                    progressDialog.dismiss();
+                }
+            }
+        });
+    }
+}
